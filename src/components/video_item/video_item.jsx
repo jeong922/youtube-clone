a676fd92517c69807207b5a4790402c11e614742
jeong922 <hwajeong922@naver.com>
2022-07-22 16:17:14 +0900

    video_list, video_item css 수정

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -2,32 +2,34 @@ import styles from './video_item.module.css';
 
 function VideoItem(props) {
   return (
-    <li className={styles.video}>
-      <div className={styles.thumbnail}>
-        <img
-          className={styles.thumbnail_img}
-          src={props.video.snippet.thumbnails.medium.url}
-          alt="video thumbnail"
-        />
-      </div>
-      <div className={styles.details}>
-        <div className={styles.wrapper}>
-          <div className={styles.channel_thumbnail}>
-            {/* <img src="" alt="" /> */}
-          </div>
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <div className={styles.thumbnail}>
+          <img
+            className={styles.thumbnail_img}
+            src={props.video.snippet.thumbnails.medium.url}
+            alt="video thumbnail"
+          />
         </div>
-        <div className={styles.info}>
-          <span className={styles.title}>
-            {props.video.snippet.title.length > 60
-              ? `${props.video.snippet.title.slice(0, 60)}...`
-              : props.video.snippet.title}
-          </span>
-          <span className={styles.channel_title}>
-            {props.video.snippet.channelTitle}
-          </span>
-          <span className={styles.publishedAt}>
-            {props.video.snippet.publishedAt.slice(0, 10)}
-          </span>
+        <div className={styles.details}>
+          <div className={styles.wrapper}>
+            <div className={styles.channel_thumbnail}>
+              {/* <img src="" alt="" /> */}
+            </div>
+          </div>
+          <div className={styles.metadata}>
+            <span className={styles.title}>
+              {props.video.snippet.title.length > 60
+                ? `${props.video.snippet.title.slice(0, 60)}...`
+                : props.video.snippet.title}
+            </span>
+            <span className={styles.channel_title}>
+              {props.video.snippet.channelTitle}
+            </span>
+            <span className={styles.publishedAt}>
+              {props.video.snippet.publishedAt.slice(0, 10)}
+            </span>
+          </div>
         </div>
       </div>
     </li>
